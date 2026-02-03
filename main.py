433070d9d3807ca5465915f94154aff4ae33ee81
Egor Muzychkin <emuzych@gmail.com>
2026-02-03 08:50:03 +0300

    refactor: move stage 1 logic to separate file

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import logging
 from pathlib import Path
 from setup_logs import setup_logs
-from docx import Document
 import tkinter as tk
 from tkinter import filedialog
 import sys
 import cProfile
 import pstats
 from index_data import index_data
-import pandas as pd
+from stage_1 import get_docx_report_for_all_UPD
 
 def get_working_directory():
     """Открывает окно выбора папки."""
@@ -37,45 +36,7 @@ def main():
 
         df = index_data(target_dir)
 
-        # 1. Список уникальных номеров УПД
-        numbers = df["Счет-фактура №"].dropna().unique().tolist()
-        # 2. Список уникальных адресов
-        consignees = df["Грузополучатель и его адрес:"].dropna().unique().tolist()
-        # 3. Общая сумма по всем документам
-        # errors='coerce' превратит мусор в NaN, чтобы sum() не сломался
-        payment_sum = pd.to_numeric(df["Всего к оплате (9)"], errors='coerce').sum()
-
-
-        payment_sum = f"{payment_sum:,.2f}"
-        logging.info(f"Количество номеров: {len(numbers)}")
-        logging.info(f"Количество грузополучателей: {len(consignees)}")
-
-        numbers_formatted = ", ".join(sorted(numbers))
-        
-        logging.info(f"Номера: {numbers_formatted}")
-        logging.info(f"Грузополучатели: {consignees}")
-        logging.info(f"Общая сумма: {payment_sum}")
-
-        # write to docx file
-        logging.info("Создаю документ docx...")
-        document = Document()
-
-        logging.info("Пишу параграф с общей суммой...")
-        document.add_paragraph(f"Общая сумма: {payment_sum}")
-
-        logging.info("Пишу параграф с номерами...")
-        document.add_paragraph(f"Номера: {numbers_formatted}")
-
-        logging.info("Пишу параграф с грузополучателями...")
-        document.add_paragraph("Грузополучатели:")
-        for c in consignees:
-            document.add_paragraph(f"– {c}")
-        
-        logging.info("Сохраняю документ...")
-        
-        output_dir = target_dir / "output"
-        output_dir.mkdir(exist_ok=True)
-        document.save(output_dir / "report.docx")
+        get_docx_report_for_all_UPD(df, target_dir)
 
         logging.info("Готово.")
     
